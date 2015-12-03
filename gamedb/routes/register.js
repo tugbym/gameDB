e23672c4a592c78ec75d7155b6eb562c9b7e39cc
tugbym <tugbym@uni.coventry.ac.uk>
2015-12-03 00:54:44 +0000

    Created basic skeleton.

---
+++ gamedb/routes/register.js
@@ -0,0 +1,10 @@
+/**
+* Created with Gaming Database.
+* User: tugbym
+* Date: 2015-12-03
+* Time: 12:48 AM
+* To change this template use Tools | Templates.
+*/
+define(function() {
+return {};
+});
