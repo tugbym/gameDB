83b89ea5e4f97eac0f8c9c87bc657be52a8eb68d
tugbym <tugbym@uni.coventry.ac.uk>
2015-12-17 04:49:39 +0000

    Updated home page

---
+++ gamedb/public/javascripts/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    document.getElementsByClassName("arrow")[0].addEventListener("click", function() {
+        window.scroll(0, 900);
+    });
+});
