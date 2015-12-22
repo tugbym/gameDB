05b19dd7bffb33c2ab075946364cd2c8f5a7d1a7
tugbym <tugbym@uni.coventry.ac.uk>
2015-12-22 00:32:13 +0000

    Multiple games can now be viewed

--- gamedb/public/javascripts/profile.js
+++ gamedb/public/javascripts/profile.js
@@ -1,38 +1,56 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     
-    var editSubmit = document.getElementById('edit-submit');
+    var gameInfos = document.getElementsByClassName('show-extras');
     
-    document.getElementById('edit-toggle').addEventListener('click', function(event) {
-        toggleDisabled();
-    });
+    for(var i = 0; i<gameInfos.length; i++) {
+        
+        document.getElementsByClassName('edit-toggle-' + i)[0].addEventListener('click', function(event) {
+            var currentIter = event.target.className,
+                pos = currentIter.split("glyphicon glyphicon-pencil edit-toggle edit-toggle-"),
+                editSubmit = document.getElementsByClassName('edit-submit-' + pos[1])[0];
+            toggleDisabled(pos[1], editSubmit, false);
+        });
+        
+        gameInfos[i].addEventListener('click', function(event) {
+            var currentIter = event.target.className,
+                pos = currentIter.split("button show-extras show-extras-"),
+                editDivs = document.getElementsByClassName('edit-' + pos[1]),
+                editToggle = document.getElementsByClassName('edit-toggle-' + pos[1])[0],
+                editSubmit = document.getElementsByClassName('edit-submit-' + pos[1])[0];
+            if(editDivs[0].style.display === "table-row") {
+                editToggle.style.display = "none";
+                event.target.innerHTML = "Show Statistics";
+                editSubmit.style.display = "none";
+                divLoop(editDivs);
+            } else {
+                toggleDisabled(pos[1], editSubmit, true);
+                event.target.innerHTML = "Hide Statistics";
+                editToggle.style.display = "block";
+                divLoop(editDivs);
+            }
+      });
+    }
     
-    document.getElementById('show-extras').addEventListener('click', function(event) {
-        var editDivs = document.getElementsByClassName('edit'),
-            editToggle = document.getElementById('edit-toggle');
+    function toggleDisabled(iter, editSubmit, alwaysDisable) {
+        var editInputs = document.getElementsByClassName('editable-item-' + iter);
+        console.log(iter, editInputs, editSubmit);
+        for(var k = 0; k<editInputs.length; k++) {
+            if(editInputs[k].disabled && !alwaysDisable) {
+                editInputs[k].disabled = false;
+                editSubmit.style.display = "table-row";
+            } else {
+                editInputs[k].disabled = true;
+                editSubmit.style.display = "none";
+            }
+        }
+    }
+    
+    function divLoop(editDivs) {
         for(var j = 0; j<editDivs.length; j++) {
             if(editDivs[j].style.display === "table-row") {
                 editDivs[j].style.display = "none";
-                event.target.innerHTML = "Show Statistics";
-                editToggle.style.display = "none";
-                toggleDisabled();
-                editSubmit.style.display = "none";
             } else {
                 editDivs[j].style.display = "table-row";
-                event.target.innerHTML = "Hide Statistics";
-                editToggle.style.display = "block";
-            }
-        }
-    });
-    
-    function toggleDisabled() {
-        var editInputs = document.getElementsByClassName('editable-item');
-        for(var i = 0; i<editInputs.length; i++) {
-            if(editInputs[i].disabled) {
-                editInputs[i].disabled = false;
-                editSubmit.style.display = "table-row";
-            } else {
-                editInputs[i].disabled = true;
-                editSubmit.style.display = "none";
             }
         }
     }
