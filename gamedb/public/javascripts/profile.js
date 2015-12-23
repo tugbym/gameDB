96353ab971750fbea5057d66dd4d02bd514dd631
tugbym <tugbym@uni.coventry.ac.uk>
2015-12-23 04:32:39 +0000

    Star rating system now working

--- gamedb/public/javascripts/profile.js
+++ gamedb/public/javascripts/profile.js
@@ -6,26 +6,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
         
         document.getElementsByClassName('edit-toggle-' + i)[0].addEventListener('click', function(event) {
             var currentIter = event.target.className,
-                pos = currentIter.split("glyphicon glyphicon-pencil edit-toggle edit-toggle-"),
-                editSubmit = document.getElementsByClassName('edit-submit-' + pos[1])[0];
-            toggleDisabled(pos[1], editSubmit, false);
+                pos = currentIter.split("edit-toggle-").pop(),
+                editSubmit = document.getElementsByClassName('edit-submit-' + pos)[0],
+                editInput = document.getElementsByClassName('editable-item-' + pos)[0];
+            toggleDisabled(pos, editSubmit, false);
+
+            if(editInput.disabled) {
+                removeStars();
+            } else {
+                iterateStars();
+            }
         });
         
         gameInfos[i].addEventListener('click', function(event) {
             var currentIter = event.target.className,
-                pos = currentIter.split("button show-extras show-extras-"),
-                editDivs = document.getElementsByClassName('edit-' + pos[1]),
-                editToggle = document.getElementsByClassName('edit-toggle-' + pos[1])[0],
-                editSubmit = document.getElementsByClassName('edit-submit-' + pos[1])[0];
+                pos = currentIter.split("show-extras-").pop(),
+                editDivs = document.getElementsByClassName('edit-' + pos),
+                editSubmit = document.getElementsByClassName('edit-submit-' + pos)[0],
+                editToggle = document.getElementsByClassName('edit-toggle-' + pos)[0],
+                stringToReplace;
+            if(editToggle.className.includes('off')) {
+                stringToReplace = editToggle.className.replace(/edit-toggle-off/, 'edit-toggle-on');
+                editToggle.className = stringToReplace;
+            } else {
+                stringToReplace = editToggle.className.replace(/edit-toggle-on/, 'edit-toggle-off');
+                editToggle.className = stringToReplace;
+            }
             if(editDivs[0].style.display === "table-row") {
-                editToggle.style.display = "none";
                 event.target.innerHTML = "Show Statistics";
                 editSubmit.style.display = "none";
                 divLoop(editDivs);
             } else {
-                toggleDisabled(pos[1], editSubmit, true);
+                toggleDisabled(pos, editSubmit, true);
                 event.target.innerHTML = "Hide Statistics";
-                editToggle.style.display = "block";
                 divLoop(editDivs);
             }
       });
@@ -33,7 +46,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
     function toggleDisabled(iter, editSubmit, alwaysDisable) {
         var editInputs = document.getElementsByClassName('editable-item-' + iter);
-        console.log(iter, editInputs, editSubmit);
         for(var k = 0; k<editInputs.length; k++) {
             if(editInputs[k].disabled && !alwaysDisable) {
                 editInputs[k].disabled = false;
@@ -52,6 +64,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
             } else {
                 editDivs[j].style.display = "table-row";
             }
+        }
+    }
+    
+    function iterateStars() {
+        var stars = document.getElementsByClassName('glyphicon-star-empty');
+    
+        for(var h = 0; h<stars.length; h++) {
+            stars[h].addEventListener('mouseover', addMouseOver);
+        }
+    }
+    
+    function removeStars() {
+        var stars = document.getElementsByClassName('star');
+        
+        for(var g = 0; g<stars.length; g++) {
+            stars[g].removeEventListener('mouseover', addMouseOver);
+        }
+        
+    }
+    
+    function addMouseOver(event) {
+        var currentIter = event.target.className,
+            iter = currentIter.split("star-").pop(),
+            iter2 = parseInt(iter, 10) + 1,
+            starsToShine,
+            starsToNotShine;
+        for(iter; 0<iter; iter--) {
+            starsToShine = document.getElementsByClassName('star-' + iter);
+            starsToShine[0].className = 'glyphicon glyphicon-star star star-' + iter;
+        }
+        for(iter2; 6>iter2; iter2++) {
+            starsToNotShine = document.getElementsByClassName('star-' + iter2);
+            starsToNotShine[0].className = 'glyphicon glyphicon-star-empty star star-' + iter2;
         }
     }
     
